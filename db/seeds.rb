fea365a13482266d3d9c6126c195831c58af10c8
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-02-22 15:46:58 +0200

    Validation and DB created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  name = Faker::Name.unique.name
+  address = Faker::Address.street_name
+  phone_number = Faker::PhoneNumber.cell_phone
+  my_array = %w(chinese italian japanese french belgian)
+  category = my_array[rand(my_array.length)]
+  Restaurant.create(name: name, address: address, phone_number: phone_number, category: category)
+end
