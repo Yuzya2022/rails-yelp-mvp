123f10d59ef61baafcac51b3b00fdac677e759b8
Olga Iuzvyshyna <iuzvyshyna.olga@gmail.com>
2023-02-22 17:49:18 +0200

    50% done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root "restaurants#index"
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # * Nested routes with relation to reviews -> /restaurants/:restaurants_id/new
+    resources :reviews, only: %i[index new create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # * /reviews/:id
+  resources :reviews, only: %i[show edit update destroy]
 end
